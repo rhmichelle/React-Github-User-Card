6621bd6e12ef5c23fdea747e47e921ec9dc61bb1
michelleRH <dev@michellerh.com>
2021-01-01 21:37:06 -0500

    Finished

--- github-user-card/src/Components/User.js
+++ github-user-card/src/Components/User.js
@@ -1,32 +1,24 @@
 import React from 'react';
+import UserFollowers from './UserFollowers'
 
 class User extends React.Component {
     constructor() {
         super();
-        this.state = {
-            userFollowers: ""
-        }
-    }
 
-    componentDidMount() {
-        fetch('https://api.github.com/users/maggieappleton/followers')
-        .then(response => response.json())
-        .then(json => {
-            this.setState({
-                userFollowers: json
-            })
-        })
     }
 
     render() {
         return(
-            <div className='UserCard'>User Div works
+            <div className='UserCard'>
+                <h2>User Card</h2>
                 <p>{this.props.user.name}</p>
                 <p>{this.props.user.blog}</p>
                 <p>{this.props.user.bio}</p>
                 <p>{this.props.user.twitter_handle}</p>
                 <p>{this.props.user.html_url}</p>
+                <UserFollowers />
             </div>
+            
         )
     }
 }
